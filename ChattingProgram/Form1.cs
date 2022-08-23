23d6007354b8e27b98fee408aee7dc3635141866
boyoung <qhdudtls97@gmail.com>
2022-08-23 22:05:37 +0900

    [Test] Button, TextBox 동작 확인

---
+++ ChattingProgram/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChattingProgram
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string strMessage;
+
+            strMessage = textBox2.Text;
+            textBox1.Text = strMessage;
+            //textBox1.Text = "Clicked Button !";
+        }
+    }
+}
